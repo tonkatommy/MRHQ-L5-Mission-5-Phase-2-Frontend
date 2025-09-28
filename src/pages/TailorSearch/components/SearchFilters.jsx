8804df28a574889b8b9902960f0d06722ce8f78d
Ben Mina <benjaminm@missionreadyhq.com>
2025-09-28 14:21:09 +1300

    MSH5-21-toggle-button-component: Made ToggleButton hidden by default and shows when Apply Filters button is clicked. Adjusts SearchFilter spacing depending on visibility of ToggleButton

--- src/pages/TailorSearch/components/SearchFilters.jsx
+++ src/pages/TailorSearch/components/SearchFilters.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SearchButton from "./filters/SearchButton";
 import SearchFiltersFuel from "./filters/SearchFiltersFuel";
 import SearchFiltersServices from "./filters/SearchFiltersServices";
@@ -5,14 +6,23 @@ import SearchFiltersStation from "./filters/SearchFiltersStation";
 import styles from "./SearchFilters.module.css";
 import ToggleButton from "./ToggleButton";
 
-function SearchFilters() {
+function SearchFilters({ onShowToggle }) {
+  const [showToggleButton, setShowToggleButton] = useState(false);
+  const handleShowToggleButton = () => {
+    setShowToggleButton(true);
+    onShowToggle?.(); //to feed Toggle status over to TailorSearch component
+  };
   return (
-    <div className={styles.container}>
+    <div
+      className={`${styles.container} ${
+        showToggleButton ? styles.containerWithToggle : styles.containerDefault
+      }`}
+    >
       <SearchFiltersServices />
       <SearchFiltersFuel />
       <SearchFiltersStation />
-      <SearchButton />
-      <ToggleButton />
+      <SearchButton onClick={handleShowToggleButton} />
+      {showToggleButton && <ToggleButton />}
     </div>
   );
 }
