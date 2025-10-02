5083ebb4a4a36159f9af422fcc4a276f208ee51b
Ben Mina <benjaminm@missionreadyhq.com>
2025-10-02 14:58:08 +1300

    MSH5-34-add-search-filters-functionality: Minor adjustments to styling

--- src/pages/FindStation/components/StationCard.jsx
+++ src/pages/FindStation/components/StationCard.jsx
@@ -19,12 +19,16 @@ const StationCard = (props) => {
     setShowMoreInfo(!showMoreInfo);
   };
   return (
-    <div className={`${styles.container} ${showMoreInfo ? styles.expanded : ""}`}>
+    <div
+      className={`${styles.container} ${showMoreInfo ? styles.expanded : ""}`}
+    >
       {/* Station Information */}
       <div className={styles.header}>
         <div className={styles.headerText}>
           <div className={styles.title}>
-            <h5 className={styles.stationName}>{stationName || "Z Stratford"}</h5>
+            <h5 className={styles.stationName}>
+              {stationName || "Z Stratford"}
+            </h5>
             <h5 className={styles.stationAddress}>
               {stationAddress || "Cnr Broadway and Regan Street, Stratford"}
             </h5>
@@ -40,7 +44,9 @@ const StationCard = (props) => {
                 src="/images/z-energy-logo-small.png"
                 alt="Z Energy Logo"
               />
-              <div className={styles.fuelTypeText}>{fuelTypes.diesel || "D"}</div>
+              <div className={styles.fuelTypeText}>
+                {fuelTypes.diesel || "D"}
+              </div>
             </div>
             <div className={styles.priceContainer}>
               <div className={styles.currencySign}>$</div>
@@ -58,11 +64,15 @@ const StationCard = (props) => {
                 src="/images/z-energy-logo-small.png"
                 alt="Z Energy Logo"
               />
-              <div className={styles.fuelTypeText}>{fuelTypes.unleaded || "91"}</div>
+              <div className={styles.fuelTypeText}>
+                {fuelTypes.unleaded || "91"}
+              </div>
             </div>
             <div className={styles.priceContainer}>
               <div className={styles.currencySign}>$</div>
-              <div className={styles.price}>{fuelPrices.unleaded || "2.35"}</div>
+              <div className={styles.price}>
+                {fuelPrices.unleaded || "2.35"}
+              </div>
             </div>
           </div>
           <div className={`${styles.fuelPremium} ${styles.fuelBox}`}>
@@ -72,7 +82,9 @@ const StationCard = (props) => {
                 src="/images/z-energy-logo-small.png"
                 alt="Z Energy Logo"
               />
-              <div className={styles.fuelTypeText}>{fuelTypes.premium || "96"}</div>
+              <div className={styles.fuelTypeText}>
+                {fuelTypes.premium || "96"}
+              </div>
             </div>
             <div className={styles.priceContainer}>
               <div className={styles.currencySign}>$</div>
@@ -83,19 +95,35 @@ const StationCard = (props) => {
       </div>
       {/* Services Icons */}
       <div className={styles.servicesIcons}>
-        <div className={`${styles.iconContainer} ${services.lpg ? "" : styles.disabled}`}>
+        <div
+          className={`${styles.iconContainer} ${
+            services.lpg ? "" : styles.disabled
+          }`}
+        >
           <img src="/images/lpg.svg" alt="LPG Bottle" />
           <div className={styles.iconLabel}>LPG swap & go</div>
         </div>
-        <div className={`${styles.iconContainer} ${services.food ? "" : styles.disabled}`}>
+        <div
+          className={`${styles.iconContainer} ${
+            services.food ? "" : styles.disabled
+          }`}
+        >
           <img src="/images/foodDrink.svg" alt="Cup with a straw" />
           <div className={styles.iconLabel}>Drinks & food</div>
         </div>
-        <div className={`${styles.iconContainer} ${services.carWash ? "" : styles.disabled}`}>
+        <div
+          className={`${styles.iconContainer} ${
+            services.carWash ? "" : styles.disabled
+          }`}
+        >
           <img src="/images/carwash.svg" alt="Carwash icon" />
           <div className={styles.iconLabel}>Car wash</div>
         </div>
-        <div className={`${styles.iconContainer} ${services.trailerHire ? "" : styles.disabled}`}>
+        <div
+          className={`${styles.iconContainer} ${
+            services.trailerHire ? "" : styles.disabled
+          }`}
+        >
           <img src="/images/trailerHire.svg" alt="Trailer icon" />
           <div className={styles.iconLabel}>Trailer hire</div>
         </div>
@@ -108,13 +136,17 @@ const StationCard = (props) => {
           <img
             src="/images/showMoreToggle.svg"
             alt="Show more options"
-            className={`${styles.toggleIcon} ${showMoreInfo ? styles.rotated : ""}`}
+            className={`${styles.toggleIcon} ${
+              showMoreInfo ? styles.rotated : ""
+            }`}
           />
         </div>
       </div>
       {/* More Information */}
       {moreInfoItems.length > 0 && (
-        <div className={`${styles.moreInfo} ${showMoreInfo ? styles.visible : ""}`}>
+        <div
+          className={`${styles.moreInfo} ${showMoreInfo ? styles.visible : ""}`}
+        >
           <div className={styles.moreInfoContent}>
             {moreInfoItems.length <= 2 ? (
               // Single column for 1-2 items
@@ -139,9 +171,13 @@ const StationCard = (props) => {
                 </div>
                 <div className={styles.moreInfoRight}>
                   <ul>
-                    {moreInfoItems.slice(Math.ceil(moreInfoItems.length / 2)).map((item, index) => (
-                      <li key={index + Math.ceil(moreInfoItems.length / 2)}>{item}</li>
-                    ))}
+                    {moreInfoItems
+                      .slice(Math.ceil(moreInfoItems.length / 2))
+                      .map((item, index) => (
+                        <li key={index + Math.ceil(moreInfoItems.length / 2)}>
+                          {item}
+                        </li>
+                      ))}
                   </ul>
                 </div>
               </>
